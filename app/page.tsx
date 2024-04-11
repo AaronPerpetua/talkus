de2dd2251596918a67a1e7ac105deaa981aec8df
Aaron Perpetua <47374220+AaronPerpetua@users.noreply.github.com>
2024-04-11 14:37:15 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,9 @@ export default async function Login() {
   }
 
   return (
- 
+  <div >
+           you&apos;re free
+      
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
@@ -45,6 +47,7 @@ export default async function Login() {
       </div>
       <div className="hidden bg-muted lg:block"></div>
     </div>
+        </div>
  
   );
 }

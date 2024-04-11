7d8c34d7ee9e3eb7abe9561752b72d5a5a8460d3
AaronPerpetua <perpetua.aaron@gmail.com>
2024-04-11 14:25:10 +0800

    change eslint settings

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,7 @@ export default async function Login() {
   }
 
   return (
+ 
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
@@ -44,5 +45,6 @@ export default async function Login() {
       </div>
       <div className="hidden bg-muted lg:block"></div>
     </div>
+ 
   );
 }
